8e10cd59ed4255bc3d3f10487aeab508da646a2b
wangms168 <wms_88@163.com>
2019-04-19 19:04:25 +0800

    再修改user-emacs-directory/init.el

--- init.el
+++ init.el
@@ -1,2 +1,2 @@
-(setq spacemacs-start-directory "~/.emacs-init/my-spacemacs/spacemacs/")
+(setq spacemacs-start-directory (concat user-emacs-directory "spacemacs/"))
 (load-file (concat spacemacs-start-directory "init.el"))
